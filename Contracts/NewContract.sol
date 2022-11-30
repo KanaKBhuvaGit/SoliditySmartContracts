7b2a30073d94bc6dfc36af632c1416886f118ca0
Kanak Bhuva <kanakbhuvagit@gmail.com>
2022-11-30 22:06:01 +0530

    Added new solidity examples

---
+++ Contracts/NewContract.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Car {
+    address public owner;
+    string public model;
+    address public carAddr;
+
+    constructor(address _owner, string memory _model) payable {
+        owner = _owner;
+        model = _model;
+        carAddr = address(this);
+    }
+}
+
+contract CarFactory {
+    Car[] public cars;
+
+    function create(address _owner, string memory _model) public {
+        Car car = new Car(_owner, _model);
+        cars.push(car);
+    }
+
+    function createAndSendEther(address _owner, string memory _model) public payable {
+        Car car = (new Car){value: msg.value}(_owner, _model);
+        cars.push(car);
+    }
+
+    function create2(
+        address _owner,
+        string memory _model,
+        bytes32 _salt
+    ) public {
+        Car car = (new Car){salt: _salt}(_owner, _model);
+        cars.push(car);
+    }
+
+    function create2AndSendEther(
+        address _owner,
+        string memory _model,
+        bytes32 _salt
+    ) public payable {
+        Car car = (new Car){value: msg.value, salt: _salt}(_owner, _model);
+        cars.push(car);
+    }
+
+    function getCar(uint _index)
+        public
+        view
+        returns (
+            address owner,
+            string memory model,
+            address carAddr,
+            uint balance
+        )
+    {
+        Car car = cars[_index];
+
+        return (car.owner(), car.model(), car.carAddr(), address(car).balance);
+    }
+
+    function getAddress (bytes memory bytecode, uint _salt)
+    public
+    view
+    returns (address)
+    {
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                bytes1(0xff), address(this), _salt, keccak256(bytecode)
+            )
+        );
+
+        return address(uint160(uint(hash)));
+    }
+
+    function getByteCode(address _owner) public pure returns (bytes memory) {
+        bytes memory bytecode = type(Car).creationCode;
+        return abi.encodePacked(bytecode, abi.encode(_owner));
+    }
+
+}
--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 - Javascript (Writing scripts)
 - [Ethers.js](https://docs.ethers.io/v5/) (Blockchain Interaction)
 - [Node.js](https://nodejs.org/en/) (To run our scripts and install ethers.js)
-- [Infura](https://infura.io/) (Node provider)
 
 ## Setting Up
 ### 1. Clone/Download the Repository
